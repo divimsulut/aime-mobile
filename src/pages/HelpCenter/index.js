e666d880de754f642098f0750e18b177c7da745d
DarellYuhu <s2200064@student.unklab.id>
2023-03-10 14:32:15 +0800

    Update Log: - fix help center styling

--- src/pages/HelpCenter/index.js
+++ src/pages/HelpCenter/index.js
@@ -126,11 +126,12 @@ export default function HelpCenter({ navigation }) {
           <View className="flex flex-col mt-10 px-6 ">
             <Text className="text-white text-left ml-2 text-xl ">FAQs</Text>
             {filter.map((item) => (
-              <View key={item.id} className="flex mt-3 overflow-y-scroll">
-                <View className="flex flex-col items-center pb-4 border-b-2 border-[#4A4848]">
-                  <View className="flex flex-row-reverse items-center justify-between w-full">
+              <View key={item.id} className="flex mt-3 overflow-y-scroll ">
+                <View className="flex flex-col items-center pb-4 border-b-2 border-[#4A4848] ">
+                  {/* Question Section */}
+                  <View className="flex  justify-between w-full ">
                     <TouchableOpacity
-                      className="items-center justify-center w-6 h-6 border border-white rounded-full"
+                      className=" absolute right-0 items-center justify-center w-6 h-6 border border-white rounded-full"
                       onPress={() =>
                         setOpenId(item.id === openId ? null : item.id)
                       }
@@ -141,12 +142,14 @@ export default function HelpCenter({ navigation }) {
                     </TouchableOpacity>
 
                     <Text
-                      className="text-white text-left ml-2 text-base "
+                      className="text-white text-left text-base mr-6 "
                       id="question"
                     >
                       {item.question}
                     </Text>
                   </View>
+
+                  {/* Answare Section */}
                   <View className="flex flex-col items-center justify-center">
                     <Text
                       className={`text-[#636363] text-left ml-2 text-base  ${
@@ -163,15 +166,17 @@ export default function HelpCenter({ navigation }) {
           </View>
 
           <View className="flex px-6 pb-28">
-            <View className="flex mt-10 h-20 w-[px] rounded-3xl bg-[#C69D0E]">
-              <View className="absolute flex flex-col items-center justify-center left-3 top-4 w-12 h-12 bg-[#002347] rounded-full">
+            <View className="flex mt-10 h-20 w-[px] justify-center flex-row items-center space-x-2 rounded-3xl bg-[#C69D0E]">
+              {/* Qustion Section */}
+              <View className=" flex flex-col items-center justify-center  w-12 h-12 bg-[#002347] rounded-full ">
                 <View className="absolute flex flex-row items-center justify-center mt-2 w-8 h-8 bg-[#001424] border border-white rounded-full">
                   <Text className="absolute text-white text-left ml-2 text-base ">
                     ?
                   </Text>
                 </View>
               </View>
-              <View className="flex flex-col items-start px-16 mt-3 justify-center">
+              {/* Text section */}
+              <View className="flex items-start  justify-center ">
                 <Text className="text-white text-left ml-2 text-base ">
                   Still have any question?
                 </Text>
@@ -179,9 +184,10 @@ export default function HelpCenter({ navigation }) {
                   Start chat with "HELP"
                 </Text>
               </View>
-              <View className="absolute right-4 bottom-4">
+              {/* Button Section */}
+              <View className="">
                 <TouchableOpacity
-                  className="flex flex-row items-center justify-center mt-2 w-[130px] h-9 border border-white rounded-2xl"
+                  className="flex flex-row items-center justify-center  w-[110px] h-9 border border-white rounded-2xl"
                   onPress={() => navigation.navigate("HelpChat")}
                 >
                   <Text
