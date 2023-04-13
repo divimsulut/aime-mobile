5a682e2673537dcaeb9e0ad930b7ce893234f5ec
jexx | roger <xa562001@gmail.com>
2023-04-13 15:54:42 +0800

    add sosmed contact center (pengganti live chat utk sementara) -roger

--- src/pages/HelpCenter/index.js
+++ src/pages/HelpCenter/index.js
@@ -6,10 +6,12 @@ import {
   TouchableOpacity,
   ScrollView,
   SafeAreaView,
+  Linking,
 } from "react-native";
 import Fontisto from "@expo/vector-icons/Fontisto";
 import { Header } from "../../components";
 import { LinearGradient } from "expo-linear-gradient";
+import { horizontalScale, verticalScale } from "../../constant";
 
 export default function HelpCenter({ navigation }) {
   const [openId, setOpenId] = React.useState(null);
@@ -39,7 +41,7 @@ export default function HelpCenter({ navigation }) {
       },
       {
         id: "2",
-        question: "how to change checkin status?",
+        question: "How to change checkin status?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 2Nunc sed semper nunc. Sed auctor, nisl sit amet aliquam lacinia, nisl nisl aliquet nisl, nec aliquam nisl nisl sit amet nisl. ",
       },
@@ -51,7 +53,7 @@ export default function HelpCenter({ navigation }) {
       },
       {
         id: "4",
-        question: "where I visited did not provide a barcode that I could scan",
+        question: "Where I visited did not provide a barcode that I could scan",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 4Nunc sed semper nunc. Sed auctor, nisl sit amet aliquam lacinia, nisl nisl aliquet nisl, nec aliquam nisl nisl sit amet nisl. ",
       },
@@ -94,16 +96,21 @@ export default function HelpCenter({ navigation }) {
             <View className="flex right-20 top-6 bg-yellow-500 opacity-80 w-2 h-2 absolute rounded-full"></View>
             <View className="flex right-20 top-9 bg-yellow-500 opacity-80 w-5 h-5 absolute rounded-full"></View>
             <View className="flex right-[70px] top-[53px] bg-yellow-500 opacity-80 w-3 h-3 absolute rounded-full"></View>
-            <View className="py-3 px-6">
-              <Text
-                className="text-white text-left ml-2 text-base "
-                id="greeting"
-              >
-                {greeting}
-              </Text>
-              <Text className="text-white text-left ml-2 text-xl  ">
-                What can we help you?
-              </Text>
+            <View style={{ zIndex: 11 }}>
+              <View className="py-3 px-6">
+                <Text
+                  className="text-white text-left ml-2 text-base "
+                  id="greeting"
+                >
+                  {greeting}
+                </Text>
+                <Text
+                  className="text-white text-left ml-2 text-xl"
+                  style={{ fontWeight: "bold" }}
+                >
+                  What can we help you?
+                </Text>
+              </View>
             </View>
           </View>
 
@@ -124,7 +131,12 @@ export default function HelpCenter({ navigation }) {
 
           {/* FAQ */}
           <View className="flex flex-col mt-10 px-6 ">
-            <Text className="text-white text-left ml-2 text-xl ">FAQs</Text>
+            <Text
+              className="text-white text-left text-xl "
+              style={{ marginBottom: 10, fontWeight: "bold" }}
+            >
+              FAQs
+            </Text>
             {filter.map((item) => (
               <View key={item.id} className="flex mt-3 overflow-y-scroll ">
                 <View className="flex flex-col items-center pb-4 border-b-2 border-[#4A4848] ">
@@ -152,7 +164,8 @@ export default function HelpCenter({ navigation }) {
                   {/* Answare Section */}
                   <View className="flex flex-col items-center justify-center">
                     <Text
-                      className={`text-[#636363] text-left ml-2 text-base  ${
+                      style={{ paddingleft: -10 }}
+                      className={`text-[#636363] text-left text-base  ${
                         item.id === openId ? "block" : "hidden"
                       }`}
                       id="answer"
@@ -165,22 +178,22 @@ export default function HelpCenter({ navigation }) {
             ))}
           </View>
 
-          <View className="flex px-6 pb-28">
-            <TouchableOpacity
+          {/* <View className="flex px-6 pb-28"> */}
+          {/* <TouchableOpacity
               activeOpacity={0.8}
               onPress={() => navigation.navigate("HelpChat")}
               className="flex mt-10 h-20 w-[px] justify-start px-4 flex-row items-center space-x-2 rounded-3xl bg-[#C69D0E]"
-            >
-              {/* Qustion Section */}
-              <View className=" flex flex-col items-center justify-center  w-12 h-12 bg-[#002347] rounded-full ">
+            > */}
+          {/* Qustion Section */}
+          {/* <View className=" flex flex-col items-center justify-center  w-12 h-12 bg-[#002347] rounded-full ">
                 <View className="absolute flex flex-row items-center justify-center mt-2 w-8 h-8 bg-[#001424] border border-white rounded-full">
                   <Text className="absolute text-white text-left ml-2 text-base ">
                     ?
                   </Text>
                 </View>
-              </View>
-              {/* Text section */}
-              <View className="flex items-start  justify-center ">
+              </View> */}
+          {/* Text section */}
+          {/* <View className="flex items-start  justify-center ">
                 <Text className="text-white text-left ml-2 text-base ">
                   Still have any question?
                 </Text>
@@ -188,7 +201,122 @@ export default function HelpCenter({ navigation }) {
                   Press to start chat now
                 </Text>
               </View>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
+          {/* </View> */}
+
+          {/* Contact Center Pusat melalui medsos (untuk sementara dulu) -roger */}
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <View
+              style={{
+                backgroundColor: "#051C2F",
+                width: horizontalScale(380),
+                marginTop: verticalScale(50),
+                padding: 20,
+                borderRadius: 10,
+                marginBottom: verticalScale(150),
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  paddingBottom: 10,
+                  textAlign: "center",
+                }}
+              >
+                Your question is not on the list?{"\n"}
+                <Text style={{ fontWeight: "bold" }}>
+                  Contact us now through our social media!
+                </Text>
+              </Text>
+              <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#25D366",
+                    padding: 10,
+                    margin: 5,
+                    borderRadius: 10,
+                    flex: 1,
+                  }}
+                  onPress={() =>
+                    Linking.openURL(
+                      `https://api.whatsapp.com/send/?phone=6285159556468`
+                    )
+                  }
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >
+                    Whatsapp
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#0088CC",
+                    padding: 10,
+                    margin: 5,
+                    borderRadius: 10,
+                    flex: 1,
+                  }}
+                  onPress={() => Linking.openURL(`https://t.me/+6287709106468`)}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >
+                    Telegram
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#00C6FF",
+                    padding: 10,
+                    margin: 5,
+                    borderRadius: 10,
+                    flex: 1,
+                  }}
+                  onPress={() => Linking.openURL(`https://m.me/alphacast.id`)}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >
+                    Messenger
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "#DD2A7B",
+                    padding: 10,
+                    margin: 5,
+                    borderRadius: 10,
+                    flex: 1,
+                  }}
+                  onPress={() => Linking.openURL(`http://ig.me/m/alphacast.id`)}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                    }}
+                  >
+                    Instagram
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
         </View>
       </ScrollView>
